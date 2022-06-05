04385a5da1e900967bc7b3c3fce71e9ebc5af608
zayan <ziyanpatiwala9@gmail.com>
2022-06-05 06:55:24 +0530

    Explicit Intent

---
+++ app/src/main/java/com/zayanpati/explicitintent/MainActivity.java
@@ -0,0 +1,29 @@
+package com.zayanpati.explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textView);
+        button=findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
